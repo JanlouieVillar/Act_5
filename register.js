6de329f69dab69afb1b3ff67a66aefac42504f4f
janlouieVillar <jlvillar010@gmail.com>
2024-03-04 09:25:56 +0800

    New commit

---
+++ register.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+
+
+
+
+let btnRegister = document.querySelector ("#btnRegister");
+let txtUsername = document.querySelector ("#txtUsername");
+let txtPassword = document.querySelector ("#txtPassword");
+let txtConfirmPassword = document.querySelector ("#txtConfirmPassword");
+let txtAge = document.querySelector ("#txtAge");
+
+
+btnRegister.onclick = function() {
+    register(txtUsername.value, txtPassword.value, txtConfirmPassword.value, txtAge.value);
+}
+
+function register (username, password, confirmPassword, age) {
+    console.log(username,  password, confirmPassword, age);
+   
+}
